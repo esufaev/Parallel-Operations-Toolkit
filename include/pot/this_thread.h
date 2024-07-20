f0b01731e86b0248448d2f0b632612588f5f9bf2
PodrivatelJop228 <gamack228@gmail.com>
2024-07-20 08:15:59 +0300

    Added ```pot::this_thread```.

---
+++ include/pot/this_thread.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <thread>
+#include <chrono>
+
+#include "executor.h"
+#include "pot/executors/thread_pool_executor.h"
+
+namespace pot::this_thread
+{
+    static std::atomic<int64_t> thread_counter{0};
+
+    thread_local int64_t id = static_cast<int64_t>(std::hash<std::thread::id>{}(std::this_thread::get_id()));
+    thread_local int64_t local_id = thread_counter++;
+
+    int64_t get_id() { return id; }
+
+    template <typename Rep, typename Period>
+    void sleep_for(const std::chrono::duration<Rep, Period> &duration)
+    {
+        std::this_thread::sleep_for(duration);
+    }
+
+    template <typename Clock, typename Duration>
+    void sleep_until(const std::chrono::time_point<Clock, Duration> &time_point)
+    {
+        std::this_thread::sleep_until(time_point);
+    }
+
+    void yileld()
+    {
+        std::this_thread::yield();
+    }
+
+    thread_local std::weak_ptr<pot::executor> executor;
+}
