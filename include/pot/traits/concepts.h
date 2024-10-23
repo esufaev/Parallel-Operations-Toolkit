20c309d5c825879ae5f4eab7c0c42eabddf68473
PEF <gamer460@mail.ru>
2024-10-24 01:05:58 +0300

    Concepts must be defined in place and contained in `namespace pot::traits`

---
+++ include/pot/traits/concepts.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <type_traits>
+
+#include <pot/coroutines/task_coroutine.h>
+
+namespace pot::traits::concepts
+{
+    // template <typename T>
+    // concept is_task = std::is_same_v<T, pot::coroutines::task<typename T::value_type>>;
+    //
+    // template <typename T>
+    // concept is_not_task = !is_task<T>;
+
+    template <typename ValueType, typename T>
+    concept is_convertible_to = std::is_convertible_v<ValueType, T>;
+}
