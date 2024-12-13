bf320d180ad33532fef2b715b4b145684e98004e
ESU <gamack228@gmail.com>
2024-12-13 16:53:27 +0300

    ```Coroutines have been reworked```

--- include/pot/coroutines/task_coroutine.h
+++ include/pot/coroutines/task_coroutine.h
@@ -10,47 +10,117 @@
 #include <type_traits>
 #include <exception>
 
-#include "pot/concepts.h"
 #include "pot/tasks/impl/shared_state.h"
 
 namespace pot::coroutines
 {
+
     template <typename T>
-    class task
+    class basic_promise_type
+    {
+    protected:
+        std::shared_ptr<tasks::details::shared_state<T>> m_state;
+        std::coroutine_handle<> m_continuation;
+
+    public:
+        std::atomic_flag m_is_resuming = ATOMIC_FLAG_INIT;
+        basic_promise_type() : m_state(std::make_shared<tasks::details::shared_state<T>>()) {}
+
+        void set_continuation(std::coroutine_handle<> continuation) noexcept
+        {
+            m_continuation = continuation;
+        }
+
+        std::coroutine_handle<> continuation() const noexcept
+        {
+            return m_continuation;
+        }
+
+        auto get_shared_state() const noexcept { return m_state; }
+
+        auto operator co_await() noexcept
+        {
+            return *this;
+        }
+
+        struct awaiter_t
+        {
+            bool await_ready() const noexcept { return false; }
+            std::coroutine_handle<> await_suspend(std::coroutine_handle<> h) noexcept
+            {
+                return h;
+            }
+            void await_resume() const noexcept {}
+        };
+
+        template <typename ValueType>
+            requires(!std::is_void_v<T> && std::is_convertible_v<ValueType, T>)
+        void set_value(ValueType &&value)
+        {
+            assert(m_state);
+            m_state->set_value(std::forward<ValueType>(value));
+        }
+
+        void set_value()
+            requires std::is_void_v<T>
+        {
+            assert(m_state);
+            m_state->set_value();
+        }
+
+        void set_exception(std::exception_ptr ex)
+        {
+            assert(m_state);
+            m_state->set_exception(ex);
+        }
+
+        static constexpr std::suspend_always initial_suspend() noexcept { return {}; }
+        static constexpr std::suspend_always final_suspend() noexcept { return {}; }
+    };
+
+    template <typename T>
+    class [[nodiscard]] task
     {
     public:
-        struct promise_type;
+        struct promise_type : public basic_promise_type<T>
+        {
+            task get_return_object()
+            {
+                return task{std::coroutine_handle<promise_type>::from_promise(*this)};
+            }
+
+            template <typename U>
+                requires std::convertible_to<U, T>
+            void return_value(U &&value)
+            {
+                this->m_state->set_value(std::forward<U>(value));
+            }
+
+            void unhandled_exception()
+            {
+                this->m_state->set_exception(std::current_exception());
+            }
+        };
+
         using handle_type = std::coroutine_handle<promise_type>;
 
-        explicit task(std::shared_ptr<pot::tasks::details::shared_state<T>> state)
-            : m_state(std::move(state)), m_handle(nullptr) requires pot::concept::is_not_task<T>
-        {
-        }
+        explicit task(handle_type h) noexcept : m_handle(h) {}
 
-        task(handle_type h) noexcept requires pot::concepts::is_not_task<T>
-            : m_handle(h) {}
-
-        task(handle_type h, std::shared_ptr<pot::tasks::details::shared_state<T>> state) noexcept requires pot::concepts::is_not_task<T>
-            : m_state(std::move(state)), m_handle(h)
+        task(task &&rhs) noexcept : m_handle(rhs.m_handle)
         {
-            h.promise().m_state = m_state;
-        }
-
-        task(task &&rhs) noexcept
-            : m_handle(std::exchange(rhs.m_handle, nullptr)), m_state(std::move(rhs.m_state))
-        {
+            rhs.m_handle = nullptr;
         }
 
         task &operator=(task &&rhs) noexcept
         {
             if (this != &rhs)
             {
-                if (m_handle && m_handle.done())
+                if (m_handle)
                 {
                     m_handle.destroy();
                 }
-                m_handle = std::exchange(rhs.m_handle, nullptr);
-                m_state = std::move(rhs.m_state);
+                m_handle = rhs.m_handle;
+                rhs.m_handle = nullptr;
             }
             return *this;
         }
@@ -60,252 +130,147 @@ namespace pot::coroutines
 
         ~task()
         {
-            if (m_handle && m_handle.done())
+            if (m_handle)
             {
                 m_handle.destroy();
             }
         }
 
-        void wait()
-        {
-            ensure_handle();
-            m_handle.promise().m_state->wait();
-        }
-
-        auto get()
-        {
-            ensure_handle();
-            if constexpr (std::is_void_v<T>)
-            {
-                m_handle.promise().m_state->get();
-            }
-            else
-            {
-                return m_handle.promise().m_state->get();
-            }
-        }
-
-        bool is_ready() const noexcept
-        {
-            return m_state && m_state->is_ready();
-        }
-
         bool await_ready() const noexcept
         {
-            return is_ready();
+            return m_handle.done();
         }
 
-        void await_suspend(std::coroutine_handle<> h) const
+        void await_suspend(std::coroutine_handle<> continuation) noexcept
         {
-            h.resume();
-        }
-
-        auto await_resume()
-        {
-            if constexpr (!std::is_void_v<T>)
+            if (m_handle && !m_handle.done() && !m_handle.promise().m_is_resuming.test_and_set(std::memory_order_acquire))
             {
-                return get();
+                m_handle.promise().set_continuation(continuation);
+                m_handle.resume();
             }
-            else
-            {
-                get();
-            }
+            m_handle.promise().m_is_resuming.clear(std::memory_order_release);
         }
 
-        T next()
+        T await_resume()
         {
-            ensure_handle();
-            if (m_handle && !m_handle.done())
+            return m_handle.promise().get_shared_state()->get();
+        }
+
+        T get()
+        {
+            if (m_handle && !m_handle.done() && !m_handle.promise().m_is_resuming.test_and_set(std::memory_order_acquire))
             {
                 m_handle.resume();
-                return m_handle.promise().m_state->get_value();
             }
-            throw std::runtime_error(std::string(std::source_location::current().function_name()) + " - Task completed, no more values.");
+            m_handle.promise().m_is_resuming.clear(std::memory_order_release);
+            return m_handle.promise().get_shared_state()->get();
+        }
+
+        class iterator
+        {
+            handle_type m_handle;
+
+        public:
+            using value_type = T;
+            using difference_type = std::ptrdiff_t;
+
+            iterator(handle_type h = nullptr) : m_handle(h) {}
+
+            value_type operator*() const { return m_handle.promise().current_value(); }
+            iterator &operator++()
+            {
+                m_handle.resume();
+                return *this;
+            }
+            bool operator==(const iterator &) const = default;
+        };
+
+        auto begin() { return iterator(m_handle); }
+        auto end() { return iterator(); }
+
+        auto operator co_await() noexcept
+        {
+            struct awaiter
+            {
+                handle_type m_handle;
+
+                bool await_ready() const noexcept
+                {
+                    return !m_handle || m_handle.done();
+                }
+
+                std::coroutine_handle<> await_suspend(std::coroutine_handle<> continuation) noexcept
+                {
+                    m_handle.promise().set_continuation(continuation);
+                    return m_handle;
+                }
+
+                T await_resume()
+                {
+                    return m_handle.promise().get_shared_state()->get();
+                }
+            };
+            return awaiter{m_handle};
         }
 
     private:
-        std::shared_ptr<pot::tasks::details::shared_state<T>> m_state;
         handle_type m_handle;
-
-        void ensure_handle(std::source_location location = std::source_location::current()) const
-        {
-            if (!m_handle)
-            {
-                throw std::runtime_error(std::string(location.function_name()) + " - Attempted to use an empty task.");
-            }
-        }
-    };
-
-    template <typename T>
-    struct task<T>::promise_type
-    {
-        std::shared_ptr<pot::tasks::details::shared_state<T>> m_state;
-
-        promise_type() : m_state(std::make_shared<pot::tasks::details::shared_state<T>>()) {}
-
-        task<T> get_return_object() requires pot::concepts::is_not_task<T>
-        {
-            return task<T>{std::coroutine_handle<promise_type>::from_promise(*this)};
-        }
-
-        std::suspend_never initial_suspend() noexcept { return {}; }
-        std::suspend_always final_suspend() noexcept { return {}; }
-
-        void return_value(T value)
-        {
-            m_state->set_value(std::move(value));
-        }
-
-        void unhandled_exception()
-        {
-            m_state->set_exception(std::current_exception());
-        }
     };
 
     template <>
-    struct task<void>::promise_type
+    struct task<void>::promise_type : basic_promise_type<void>
     {
-        std::shared_ptr<pot::tasks::details::shared_state<void>> m_state;
-
-        promise_type() : m_state(std::make_shared<pot::tasks::details::shared_state<void>>()) {}
-
         task<void> get_return_object()
         {
             return task<void>{std::coroutine_handle<promise_type>::from_promise(*this)};
         }
 
-        std::suspend_never initial_suspend() noexcept { return {}; }
-        std::suspend_always final_suspend() noexcept { return {}; }
-
         void return_void()
         {
-            m_state->set_value();
+            this->m_state->set_value();
         }
 
         void unhandled_exception()
         {
-            m_state->set_exception(std::current_exception());
+            this->m_state->set_exception(std::current_exception());
         }
     };
-
-    template <typename T>
-    class promise
-    {
-    public:
-        promise() : m_state(std::make_shared<pot::tasks::details::shared_state<T>>()) {}
-
-        template<typename ValueType>
-        requires std::is_convertible_v<ValueType, T>
-        void set_value(ValueType&& value)
-        {
-            ensure_state();
-            m_state->set_value(std::forward<ValueType>(value));
-        }
-
-        void set_value() requires std::is_void_v<T>
-        {
-            ensure_state();
-            m_state->set_value();
-        }
-
-        void set_exception(std::exception_ptr ex)
-        {
-            ensure_state();
-            m_state->set_exception(ex);
-        }
-
-        task<T> get_task()
-        {
-            return task<T>{m_state};
-        }
-
-    private:
-        std::shared_ptr<pot::tasks::details::shared_state<T>> m_state;
-
-        void ensure_state() const
-        {
-            if (!m_state)
-            {
-                throw std::runtime_error("promise: operation on an uninitialized state");
-            }
-        }
-    };
-
 } // namespace pot::coroutines
-
 
 namespace pot::traits
 {
-    template<typename T>
+    template <typename T>
     struct task_value_type;
-    template<typename ValueType>
+
+    template <typename ValueType>
     struct task_value_type<coroutines::task<ValueType>>
     {
         using type = ValueType;
     };
 
-    template<typename ValueType>
+    template <typename ValueType>
     using task_value_type_t = typename task_value_type<ValueType>::type;
 
-    template<typename T>
-    struct awaitable_value_type {
+    template <typename T>
+    struct awaitable_value_type
+    {
         using type = T;
     };
 
-    template<template<typename> typename Task, typename T>
-    struct awaitable_value_type<Task<T>> {
+    template <template <typename> typename Task, typename T>
+    struct awaitable_value_type<Task<T>>
+    {
         using type = T;
     };
 
-    template<typename T>
+    template <typename T>
     using awaitable_value_type_t = typename awaitable_value_type<T>::type;
-
 
     namespace concepts
     {
-        template<typename Type>
-        concept is_task = requires(Type t)
-        {
+        template <typename Type>
+        concept is_task = requires(Type t) {
             typename task_value_type_t<std::remove_cvref_t<Type>>;
-            // typename Type::promise_type;
-            // { t.operator co_await() };
-
         };
-
-        template<typename Type>
-        concept is_not_task = !is_task<Type>;
-
-        template<typename Type>
-        concept awaitable = requires(Type t, std::coroutine_handle<> h)
-        {
-            { t.await_ready() } -> std::convertible_to<bool>;
-            { t.await_suspend(h) } -> std::same_as<void>;
-            { t.await_resume() };
-        };
-
-        template<typename Func, typename... Args>
-        concept is_coroutine = requires(Func f, Args... args)
-        {
-            { f(args...) } -> is_task;
-        };
-
     }
-    static_assert(std::is_same_v<task_value_type_t<coroutines::task<int>>, int>);
-    static_assert(concepts::is_task<coroutines::task<int>>);
-    static_assert(concepts::is_not_task<int>);
-
-    static_assert(concepts::awaitable<coroutines::task<int>>);
-    // static_assert(!concepts::awaitable<coroutines::promise<int>>);
-    static_assert(!concepts::awaitable<int>);
-
-    static_assert(concepts::is_coroutine<decltype([]() -> coroutines::task<void> { co_return  ; })> );
-    static_assert(concepts::is_coroutine<decltype([]() -> coroutines::task<int > { co_return 0; })> );
-
-    static_assert(concepts::is_coroutine<decltype([](int) -> coroutines::task<void> { co_return ; }), int> );
-    static_assert(concepts::is_coroutine<decltype([](int) -> coroutines::task<int > { co_return 0; }), int> );
-    static_assert(!concepts::is_coroutine<decltype([](int) -> coroutines::task<int> { co_return 0; })> );
-    static_assert(!concepts::is_coroutine<decltype([](int) -> int { return 0; })> );
 }
-
-
-
--- include/pot/tasks/impl/shared_state.h
+++ include/pot/tasks/impl/shared_state.h
@@ -14,137 +14,92 @@
 
 namespace pot::tasks::details
 {
+
     template <typename T>
     class shared_state
     {
     public:
+        using value_type = T;
         using variant_type = std::conditional_t<std::is_void_v<T>,
-            std::variant<std::monostate, std::exception_ptr>,
-            std::variant<std::monostate, T, std::exception_ptr>>;
+                                                std::variant<std::monostate, std::exception_ptr>,
+                                                std::variant<std::monostate, T, std::exception_ptr>>;
 
         shared_state() = default;
 
-        shared_state(const shared_state& other)
-        {
-            m_variant = other.m_variant;
-            m_ready.store(other.m_ready.load(std::memory_order_acquire), std::memory_order_release);
-        }
-
-        shared_state& operator=(const shared_state& other)
-        {
-            if (this != &other)
-            {
-                m_variant = other.m_variant;
-                m_ready.store(other.m_ready.load(std::memory_order_acquire), std::memory_order_release);
-            }
-            return *this;
-        }
-
-        template<typename U = T>
-        void set_value() requires (std::is_void_v<U>)
-        {
-            if (m_ready.exchange(true, std::memory_order_release))
-            {
-                throw std::runtime_error("shared_state<void>::set_value() - value already set.");
-            }
-
-            m_variant = std::monostate{};
-            notify_completion();
-        }
-
-        template<typename U = T>
-        void set_value(U&& value) requires (!std::is_void_v<U>)
-        {
-            if (m_ready.exchange(true, std::memory_order_release))
-            {
-                throw std::runtime_error("shared_state::set_value() - value already set.");
-            }
-
-            m_variant.template emplace<T>(std::forward<U>(value));
-            // m_variant = std::variant<T>(std::forward<U>(value));
-            notify_completion();
-        }
-
-        void set_exception(std::exception_ptr eptr)
-        {
-            if (m_ready.exchange(true, std::memory_order_release))
-            {
-                throw std::runtime_error("shared_state::set_exception() - exception already set.");
-            }
-
-            m_variant = eptr;
-            notify_completion();
-        }
+        shared_state(const shared_state &) = delete;
+        shared_state &operator=(const shared_state &) = delete;
 
         template <typename U = T>
-            requires (!std::is_void_v<U>)
-        U get()
+        void set_value(U &&value)
+            requires(!std::is_void_v<T>)
         {
-            wait();
-            if (std::holds_alternative<std::exception_ptr>(m_variant))
+            m_data.template emplace<T>(std::forward<U>(value));
+            if (m_ready.exchange(true, std::memory_order_release))
             {
-                std::rethrow_exception(std::get<std::exception_ptr>(m_variant));
+                throw std::runtime_error("Value already set in shared_state.");
             }
-            if (std::holds_alternative<std::monostate>(m_variant))
-            {
-                throw std::runtime_error("shared_state::get() - no value set.");
-            }
-            return std::get<U>(m_variant);
+            notify_continuation();
         }
 
-        template <typename U = void>
-            requires (std::is_void_v<U>)
-        void get()
+        void set_value()
+            requires(std::is_void_v<T>)
+        {
+            m_data = std::monostate{};
+            if (m_ready.exchange(true, std::memory_order_release))
+            {
+                throw std::runtime_error("Value already set in shared_state.");
+            }
+            notify_continuation();
+        }
+
+        void set_exception(std::exception_ptr ex)
+        {
+            m_data = ex;
+            if (m_ready.exchange(true, std::memory_order_release))
+            {
+                throw std::runtime_error("Exception already set in shared_state.");
+            }
+            notify_continuation();
+        }
+
+        T get()
         {
             wait();
-            if (std::holds_alternative<std::exception_ptr>(m_variant))
+            if (std::holds_alternative<std::exception_ptr>(m_data))
             {
-                std::rethrow_exception(std::get<std::exception_ptr>(m_variant));
+                std::rethrow_exception(std::get<std::exception_ptr>(m_data));
             }
+            if constexpr (!std::is_void_v<T>)
+            {
+                return std::get<T>(m_data);
+            }
+        }
+
+        void set_continuation(std::coroutine_handle<> continuation) noexcept
+        {
+            m_continuation = continuation;
         }
 
         void wait() const
         {
             while (!m_ready.load(std::memory_order_acquire))
             {
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                std::this_thread::yield();
             }
-        }
-
-        template <typename Rep, typename Period>
-        bool wait_for(const std::chrono::duration<Rep, Period>& timeout_duration)
-        {
-            const auto start_time = std::chrono::steady_clock::now();
-            while (!m_ready.load(std::memory_order_acquire))
-            {
-                if (std::chrono::steady_clock::now() - start_time > timeout_duration)
-                {
-                    return false;
-                }
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            }
-            return true;
-        }
-
-        template <typename Clock, typename Duration>
-        bool wait_until(const std::chrono::time_point<Clock, Duration>& timeout_time)
-        {
-            return wait_for(timeout_time - std::chrono::steady_clock::now());
-        }
-
-        bool is_ready() const noexcept
-        {
-            return m_ready.load();
         }
 
     private:
-        void notify_completion()
+        void notify_continuation()
         {
-            // m_ready.store(true, std::memory_order_acq_rel);
-            m_ready = true;
+            if (m_continuation)
+            {
+                m_continuation.resume();
+            }
         }
 
-        std::atomic<bool> m_ready{ false };
-        variant_type m_variant{ std::monostate{} };
+        std::atomic<bool> m_ready{false};
+        variant_type m_data{std::monostate{}};
+        std::coroutine_handle<> m_continuation{nullptr};
     };
-}
+
+} // namespace pot::tasks::details
